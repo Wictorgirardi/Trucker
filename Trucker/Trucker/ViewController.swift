2acee14ef289ef5bae005ef1ef085f94d45e296b
wictorgirardi <wictor.girardi@gmail.com>
2019-07-23 23:15:45 -0300

    Criando tela de boas vindas

--- Trucker/Trucker/ViewController.swift
+++ Trucker/Trucker/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var cadastroButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginButton.layer.cornerRadius = 7
+        
+        cadastroButton.layer.shadowColor = UIColor.gray.cgColor
+        cadastroButton.layer.shadowRadius = 10
+        cadastroButton.layer.shadowOpacity = 0.25
+        cadastroButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+        cadastroButton.layer.cornerRadius = 7
+        
+        
+        
+        
     }
 
 
