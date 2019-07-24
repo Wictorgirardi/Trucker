df9a4f23fe613d1b5be79be71ae49ff691c38937
wictorgirardi <wictor.girardi@gmail.com>
2019-07-24 01:08:15 -0300

    Criando telas de login e cadastro

--- Trucker/Trucker/ViewController.swift
+++ Trucker/Trucker/ViewController.swift
@@ -9,26 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var cadastroButton: UIButton!
+    @IBAction func cadastro2(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loginButton.layer.cornerRadius = 7
-        
-        cadastroButton.layer.shadowColor = UIColor.gray.cgColor
-        cadastroButton.layer.shadowRadius = 10
-        cadastroButton.layer.shadowOpacity = 0.25
-        cadastroButton.layer.shadowOffset = CGSize(width: 0, height: 0)
-        cadastroButton.layer.cornerRadius = 7
-        
-        
-        
+        loginButton.applyDesign2()
+        cadastroButton.applyDesign()
         
     }
-
-
 }
 
+extension UIButton {
+    func applyDesign() {
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowRadius = (10)
+        self.layer.shadowOpacity = (0.25)
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.cornerRadius = (7)
+        self.layer.cornerRadius = (7)
+    }
+    func applyDesign2() {
+        self.layer.cornerRadius = (7)
+    }
+}
